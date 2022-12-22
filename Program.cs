d14d9a8299b71b2f10747c720eb84d3df4e8eb11
Алексей Степанов <sayu94@yandex.ru>
2022-12-22 14:14:00 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsHW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\Users\\M-015\\Downloads\\Text1.txt";//путь к файлу
+            string readText = File.ReadAllText(filePath);
+            //Проверяем производительность вставки в обычный список
+            var stopWatch = Stopwatch.StartNew();
+            List<string> SimpleList = new List<string>();
+            foreach (string line in readText.Split(new char[] { '\n', '\r' }))
+            {
+                SimpleList.Add(line);
+            }
+            Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+
+            //Проверяем производительность вставки в связный список
+            var stopWatch1 = Stopwatch.StartNew();
+            LinkedList<string> LinkedList = new LinkedList<string>();
+            foreach (string line in readText.Split(new char[] { '\n', '\r' }))
+            {
+                LinkedList.AddLast(line);
+            }
+            Console.WriteLine(stopWatch1.Elapsed.TotalMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
